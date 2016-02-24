e821cf4ba1664de9916d7687608e26d26015b19f
Josh <engelsjo@mail.gvsu.edu>
2016-02-24 12:44:34 -0500

    further work on parsing the input files

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -5,10 +5,27 @@
 import sys
 
 class TrainingExample(object):
-    def __init__(self, attributesAndValues, targetValue):
-        self.attributesAndValues = attributesAndValues
+    def __init__(self, attributes, targetValue):
+        # self.attributes will be a list of Attribute objects where each attribute has a list of 1 value
+        self.attributes = attributes
         self.targetValue = targetValue
 
+    def __str__(self):
+        return "Training Values: {}\nTargetValues: {}\n".format(self.attributes, self.targetValue)
+
+    def __repr__(self):
+        return self.__str__()
+
+class Attribute(object):
+    def __init__(self, attrName, attrValues):
+        self.attrName = attrName
+        self.attrValues = attrValues
+
+    def __str__(self):
+        return "Attribute Name: {}\nAttributeValues: {}\n".format(self.attrName, self.attrValues)
+
+    def __repr__(self):
+        return self.__str__()
 
 class DecisionTree(object):
     def __init__(self, nbrOfTargets, targetNames, nbrOfAttributes, attributesAndValues, nbrOfTrainingExamples, trainingExamples):
@@ -29,13 +46,47 @@ class DecisionTree(object):
 def main(argv):
     if len(argv) < 1:
         print(usage())
-    dt = DecisionTree()
+    dataFilePath = argv[1]
+    with open(dataFilePath, "r") as fh:
+        nbrOfTargetsLine = fh.readline().strip()
+        targetValuesLine = fh.readline().strip()
+        nbrOfAttributes = int(fh.readline().strip())
+        
+        # build a list of attribute objects holding the attrName and attrValues
+        attributes = [Attribute(None, None)] * nbrOfAttributes
+        for attrNum in range(nbrOfAttributes):
+            attrLine = fh.readline().strip().split("A:")[1]
+            attrLineParts = attrLine.split()
+            attrName = attrLineParts[0]
+            attributes[attrNum].attrName = attrName
+            attrValues = []
+            for i in range(2, len(attrLineParts)):
+                attrValues.append(attrLineParts[i])
+            attributes[attrNum].attrValues = attrValues
+                
+        # build a list of all the example data
+        nbrOfExamples = int(fh.readline().strip())
+        examples = []
+        for i in range(nbrOfExamples):
+            exampleLine = fh.readline().strip().split("D:")[1]
+            exampleLineParts = exampleLine.split()
+            targetValue = exampleLineParts[-1]
+            exampleAttributes = [Attribute(None, None)] * nbrOfAttributes
+
+
+
+
+    #dt = DecisionTree()
 
 def usage():
     """
-    print out how to operate the program
+    return str of how to operate the program
     """
-    
+    return """
+    python decision_tree.py [dataFile]
+        [dataFile] - the path to the file that will be used to build the tree
+    """
+
 
 
 
