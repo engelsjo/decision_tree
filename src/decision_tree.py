d6ee24f9b258aa4782d1fdf9db0fbddd1bf926cf
Josh <engelsjo@mail.gvsu.edu>
2016-02-24 09:46:20 -0500

    initial setup commit

---
+++ src/decision_tree.py
@@ -0,0 +1,26 @@
+"""
+@authors: Michael Baldwin, Josh Engelsma, Adam Terwilliger
+"""
+
+import sys
+
+
+class DecisionTree(object):
+    def __init__(self):
+        self.dataFilePath = ""
+        self.rootNode = None
+        # build our tree when this class is init
+        self.buildTree()
+
+    def buildTree(self):
+        """
+        method to build our tree
+        """
+
+def main(argv):
+    dt = DecisionTree()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
