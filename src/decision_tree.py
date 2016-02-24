beb2a4bb1681a8d6d220c5b59eebe1ad5cd4f9d4
Josh <engelsjo@mail.gvsu.edu>
2016-02-24 09:58:24 -0500

    some more initial framework setup

--- src/decision_tree.py
+++ src/decision_tree.py
@@ -4,21 +4,38 @@
 
 import sys
 
+class TrainingExample(object):
+    def __init__(self, attributesAndValues, targetValue):
+        self.attributesAndValues = attributesAndValues
+        self.targetValue = targetValue
+
 
 class DecisionTree(object):
-    def __init__(self):
-        self.dataFilePath = ""
-        self.rootNode = None
-        # build our tree when this class is init
+    def __init__(self, nbrOfTargets, targetNames, nbrOfAttributes, attributesAndValues, nbrOfTrainingExamples, trainingExamples):
+        self.nbrOfTargets = nbrOfTargets
+        self.targetNames = targetNames
+        self.nbrOfAttributes = nbrOfAttributes
+        self.attributesAndValues = attributesAndValues
+        self.nbrOfTrainingExamples = nbrOfTrainingExamples
+        self.trainingExamples = trainingExamples
         self.buildTree()
 
     def buildTree(self):
         """
         method to build our tree
         """
+        self.rootNode = None
 
 def main(argv):
+    if len(argv) < 1:
+        print(usage())
     dt = DecisionTree()
+
+def usage():
+    """
+    print out how to operate the program
+    """
+    
 
 
 
