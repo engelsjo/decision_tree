b962ecb3deef5e79f0446239329066ee21c53945
Adam Terwilliger <adamtwig@hotmail.com>
2016-03-12 18:33:16 -0500

    update numeric dt

--- src/numericDT_AT.py
+++ src/numericDT_AT.py
@@ -4,15 +4,30 @@
 """
 
 import sys
+import numpy as np
 
 def main(argv):
     if len(argv) < 2:
         print(usage())
     dataFilePath = argv[1]
+    #irisDict = {}    
+    irisMatrix = np.zeros((150,4))
     with open(dataFilePath, "r") as fh:
+    	i = 0
     	for line in fh:
         #print fh.readline().strip()
-        	print line
+        	lineParts = line.strip("\n").split(",")
+        	#irisDict[(lineParts[4], i)] = [float(lineParts[0]), float(lineParts[1]),
+        	#							float(lineParts[2]), float(lineParts[3])]
+        	for j in range(4):
+        		irisMatrix[i,j] = float(lineParts[j])
+        	i +=1
+    
+    a = irisMatrix
+    b = a[a[:,0].argsort()]
+    print irisMatrix[0]
+    print b
+
 
 def usage():
     return """
